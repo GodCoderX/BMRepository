5fb4b9dac746e4213393a320b7cd907afef402b4
GodCoderX <godcoderx@gmail.com>
2016-10-29 19:06:36 +0900

    update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -21,33 +21,33 @@ if [ ! -e Data ]; then
 fi
 
 cwd=`dirname ${0}`
-expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)` # cwd=/path/to/Default
+expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 cd ${cwd}
-find Mod -name "*.zip" | while read ModZip # ModZip="Mod/gfx.drivearrow.transparent50.zip"
+find Mod -name "*.zip" | while read ModZip
 do
 	echo "Processing "${ModZip}" ..."
 	ModDir="Mod"
-	OriZip=`echo "Original/"${ModZip#*/}` # OriZip="Original/gfx.drivearrow.transparent50.zip"
+	OriZip=`echo "Original/"${ModZip#*/}`
 	OriDir="Original"
 	cd ${cwd}
 	cd ${ModDir} # cd "Mod"
 	unzip ${ModZip#*/} > /dev/null
-	find Data -type f | while read FileName # FileName="Data/Sfx/GUI_battle_streamed.fsb"
+	find Data -type f | while read FileName
 	do
 		cd ${cwd}
 		echo "-- "${ModDir}"/"${FileName}
 		FileName=`escape ${FileName}`
 		if [ -e ${FileName} ]; then
-			FilePath="${OriDir}/${FileName}" # FilePath="Original/1/6/5/Data/Sfx/GUI_battle_streamed.fsb"
-			mkdir -p "${FilePath%/*}" # mkdir -p Original/1/6/5/Data/Sfx
-			cp "${FileName}" "${FilePath}" # cp "Data/Sfx/GUI_battle_streamed.fsb" "Original/1/6/5/Data/Sfx/GUI_battle_streamed.fsb"
+			FilePath="${OriDir}/${FileName}"
+			mkdir -p "${FilePath%/*}"
+			cp "${FileName}" "${FilePath}"
 		fi
 	done
 	cd ${cwd}
-	cd ${ModDir} # cd "Mod/1/6/5"
+	cd ${ModDir}
 	rm -rf Data
 	cd ${cwd}
-	cd ${OriDir} # cd "Original/1/6/5"
+	cd ${OriDir}
 	echo "Archiving ${ModZip%/*}"
 	zip -r ${ModZip#*/} Data > /dev/null
 	rm -rf Data
