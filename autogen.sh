5b4f032295b33227fc3ca33e354aea880054887a
GodCoderX <godcoderx@gmail.com>
2016-10-31 02:13:05 +0900

    update

--- autogen.sh
+++ autogen.sh
@@ -5,13 +5,10 @@ escape() {
 	echo ${escaped}
 }
 
-if [ -e Original ]; then
+if [ -e Remove ]; then
 	echo "Initializing started."
-	rm -rf Original
-	find Mod -name "Data" -type d | while read DataPath
-	do
-		rm -rf ${DataPath}
-	done
+	rm -rf Remove
+	mkdir Remove
 	echo "Initializing finished."
 fi
 
@@ -23,33 +20,33 @@ fi
 cwd=`dirname ${0}`
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
 cd ${cwd}
-find Mod -name "*.zip" | while read ModZip
+find Install -name "*.zip" | while read InstallZip
 do
-	echo "Processing "${ModZip}" ..."
-	ModDir="Mod"
-	OriZip=`echo "Original/"${ModZip#*/}`
-	OriDir="Original"
+	echo "Processing "${InstallZip}" ..."
+	InstallDir="Install"
+	RemoveZip=`echo "Remove/"${InstallZip#*/}`
+	RemoveDir="Remove"
 	cd ${cwd}
-	cd ${ModDir} # cd "Mod"
-	unzip ${ModZip#*/} > /dev/null
+	cd ${InstallDir}
+	unzip ${InstallZip#*/} > /dev/null
 	find Data -type f | while read FileName
 	do
 		cd ${cwd}
-		echo "-- "${ModDir}"/"${FileName}
+		echo "-- "${InstallDir}"/"${FileName}
 		FileName=`escape ${FileName}`
 		if [ -e ${FileName} ]; then
-			FilePath="${OriDir}/${FileName}"
+			FilePath="${RemoveDir}/${FileName}"
 			mkdir -p "${FilePath%/*}"
 			cp "${FileName}" "${FilePath}"
 		fi
 	done
 	cd ${cwd}
-	cd ${ModDir}
+	cd ${InstallDir}
 	rm -rf Data
 	cd ${cwd}
-	cd ${OriDir}
-	echo "Archiving ${ModZip%/*}"
-	zip -r ${ModZip#*/} Data > /dev/null
+	cd ${RemoveDir}
+	echo "Archiving ${InstallZip%/*}"
+	zip -r ${InstallZip#*/} Data > /dev/null
 	rm -rf Data
 	cd ${cwd}
 done
